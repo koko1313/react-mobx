634b4124706ce234cca9023252da8f30f9ecd776
koko1313 <kaloyanvellichkov@gmail.com>
2020-05-20 11:26:30 +0300

    added an action

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -4,12 +4,23 @@ import { observer, inject } from 'mobx-react';
 @observer
 class Collection extends React.Component {
 
-    render() {
+    addContact = () => {
+        this.props.contacts.add({id: 3, name: "person 3", email: "p3@mail.com"});
+    }
+
+    renderContacts = () => {
         return this.props.contacts.all.map((contact, index) => {
             return <p key={index}>{contact.email}</p>;
         });
     }
-    
+
+    render() {
+        return <>
+            {this.renderContacts()}
+            <button onClick={this.addContact}>Add contact</button>
+        </>;
+    }
+
 }
 
 export default inject("contacts")(Collection);
--- src/stores/Contacts.js
+++ src/stores/Contacts.js
@@ -1,10 +1,14 @@
-import { observable } from 'mobx';
+import { observable, action } from 'mobx';
 
 class Contacts {
     @observable all = [
         {id: 1, name: 'Person 1', email: 'p1@mail.com'},
         {id: 2, name: 'Person 2', email: 'p2@mail.com'},
     ];
+
+    @action add(person) {
+        this.all.push({id: person.id, name: person.name, email: person.email});
+    }
 }
 
 const store = new Contacts();
